41df0265073d5b4c33fced5f4c78f5eb9e3535db
Jason McNeill <vrtjason@gmail.com>
2017-10-03 07:00:33 -0700

    Initial commit

---
+++ js/all.js
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function Everything() {
+
+	// No page transition effects
+	$.mobile.defaultPageTransition = 'none';
+
+	// Function:  Turn off audio
+	var turnOffAudio = function(){
+		var audioElements = document.getElementsByTagName('audio');
+		if(audioElements.length >= 1){
+			for(i=0; i<audioElements.length; i++){
+				audioElements[i].pause();
+			}			
+		}	
+	};
+
+	var callRemote = function(event, db, jsonpurl){
+		if(event) var event = JSON.stringify(event) || {};
+		if(db) var db = JSON.stringify(db) || {};
+		if(jsonpurl) var jsonpurl = jsonpurl || "http://www.usd21.org/m/fp/remote/?";
+		if(navigator.onLine){
+			$.ajax({
+				url: jsonpurl,
+				jsonp: "callback",
+				dataType: "jsonp",
+				data: {
+					"event": event,
+					"db": db,
+					"jsonpurl": jsonpurl
+				},
+				success: function(response) {
+				},
+				error: function(err){
+					alert(err);
+				}
+			});
+		}
+	};
+
+	$(document).on('pagecreate', function(event) {
+
+		turnOffAudio();
+		var db = new PouchDB('firstPrinciples.db', {
+			adapter: 'cordova-sqlite',
+			iosDatabaseLocation: 'Library',
+			androidDatabaseImplementation: 2
+		});
+		var dbInfo = db.info();
+		callRemote(event, dbInfo);
+		alert("pageinit event has fired");
+
+	});
+
+}
+
+var app = {
+
+	initialize: function() {
+		document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+	},
+
+	onDeviceReady: function() {
+		window.plugin.statusbarOverlay.isVisible( function (isVisible) {
+			window.plugin.statusbarOverlay.hide();
+		});
+		
+		$("a").on("click", function(evt){
+			var url = evt.currentTarget.href;
+			if( (url.indexOf("http://")>=0) || (url.indexOf("https://")>=0) ) {
+				evt.preventDefault();
+				if (! navigator.onLine) {
+					return alert("You must be connected to the internet in order to access this link.");
+				}
+				if( url.indexOf("usd21.org/m/niv84")>=0 ) {
+					var target = "_self";
+					var ref = cordova.InAppBrowser.open(url, target, 'location=yes,hardwareback=yes,shouldPauseOnSuspend=yes,allowInlineMediaPlayback=yes');
+				} else {
+					var target = "_system";
+					var ref = cordova.InAppBrowser.open(url, target, 'location=yes,hardwareback=yes,shouldPauseOnSuspend=yes,allowInlineMediaPlayback=yes');
+				}
+			}
+		});
+
+		Everything();
+	}
+
+};
+
+app.initialize();
